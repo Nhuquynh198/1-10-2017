7977fec2baca31d156ce2e992036882392ae965c
Nhu Quynh <kieuquynh198@gmial.com>
2017-10-01 22:08:28 +0700

    quynh add file

---
+++ Untitled-1.php
@@ -0,0 +1,66 @@
+/* <div class="container">
+    	<div class="col-lg-6 col-md-6">
+        	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+            
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <a href="#" class="clearfix">
+                    	<img src="img/slide1.png" width="573" height="502" alt="Sile 1" style="width:100%;">
+                    </a>
+                  </div>
+            
+                  <div class="item">
+                     <a href="#" class="clearfix">
+                     	<img src="img/slide2.png" width="573" height="502" alt="Slide 2" style="width:100%;">
+                     </a>
+                  </div>
+                
+                  <div class="item">
+                     <a href="#" class="clearfix">
+                     	<img src="img/slide3.png" width="573" height="502" alt="Slide 3" style="width:100%;">
+                     </a>
+                  </div>
+           		</div>
+        
+    <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+          	</div>
+            <div class="">
+            	
+            </div>
+        </div>
+    	<div class="col-lg-6 col-md-6">
+        	<a href="#" class="clearfix">
+            	<img src="img/q110.png" width="580" height="220"title="Kiều Thị Như Quỳnh">
+            </a>
+            
+            <div class="col-lg-6 col-md-6" style="padding-top:13px; padding-left:1px" >
+            	<a href="#" class="clearfix" title="Kiều Thị Như Quỳnh">
+            		
+					<img src="img/q60.png" width="270" height="240">
+            	</a>
+                
+            </div>
+            <div class="col-lg-6 col-md-6" style="padding-top:13px; padding-left:40px">
+            	<a href="#" class="clearfix" title="Kiều Thị Như Quỳnh">
+            		<img src="img/q60.png" width="270" height="240">
+            	</a>
+                
+            </div>
+           
+        </div>
+    </div> */
